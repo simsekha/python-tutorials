2cd5f1e603830bd76cd0285a64aa7c8dc414b6c8
hayri şimşek <simsekha@hotmail.com>
2018-12-06 00:00:19 +0300

    first commit

---
+++ HelloWorld/app.py
@@ -0,0 +1,23 @@
+import math
+# comment
+print("Hello World")
+print("_"*25)
+print("Hello World")
+x = 3.0000
+y = 5444
+print(x + y)
+arr = ["asd", 2, True]
+arr2 = [1, 2, 3, 4, 5]
+im = 3 + 3j
+
+print(im**-1)
+
+print([arr, arr2])
+str = "selamlar \"eyy ganiii"
+print(str[:])
+for target_list in arr:
+    print(target_list)
+    pass
+else:
+    print(target_list)
+    pass
